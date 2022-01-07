2724077efd3f0eed59f42a70555849dbc4eb5d13
Elliot B <elliot@elliotb.com>
2022-01-07 16:52:43 -0500

    Error handling?

--- bot/main.py
+++ bot/main.py
@@ -23,25 +23,30 @@ async def on_member_join(member):
 	statusChannel = discord.utils.get(guild.text_channels, name=_status_messages_channel)
 	await statusChannel.send(f"Hey! {member.mention} just joined the server!")
 
-
 @bot.event
 async def on_member_update(before, after):
 	if before.pending == True and after.pending == False:
 		guild = after.guild
 		print(f"User '{after.display_name}' ({after.name}) in server '{guild.name}' went through screening. Applying '{_default_user_role}' role.")
 
-		role = discord.utils.get(after.guild.roles, name=_default_user_role)
-		await after.add_roles(role, reason = "Completed member screening.")
+		try:
+			role = discord.utils.get(after.guild.roles, name=_default_user_role)
+			await after.add_roles(role, reason = "Completed member screening.")
+		except discord.errors.Forbidden as e:
+			# Send a message to the server about it, and then re-raise it so it gets logged
+			# on the server.
+			statusChannel = discord.utils.get(guild.text_channels, name=_status_messages_channel)
+			await statusChannel.send(f"ERROR! Bot got a 'FORBIDDEN' error after trying to grant the '{_default_user_role}' role to '{after.mention}'!")
+			raise e
+
 
 		statusChannel = discord.utils.get(guild.text_channels, name=_status_messages_channel)
 		await statusChannel.send(f"{after.mention} completed screening and was given the '{_default_user_role}' role!")
 		print(f"Gave role '{_default_user_role}' to user '{after.display_name}'.")
 
-
-
 @bot.command()
 async def ping(ctx):
-	await ctx.send("pong")
+	await ctx.send("pong")	
 
 if __name__ == "__main__":
 	bot.run(TOKEN)
