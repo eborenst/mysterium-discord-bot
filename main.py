17deac040a18efd64bbf32faee71f45d82792635
Elliot B <elliot@elliotb.com>
2023-05-21 15:33:22 -0400

    Added Odo's code, with some readability tweaks. Fails on urlopen though.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 import discord
 from discord.ext import commands
+from urllib.request import urlopen
 
 # ------- CONSTANTS -------
 _default_user_role = "Guildsman" # The role to grant after Member Screening.
@@ -14,7 +15,6 @@ intents.members = True
 intents.message_content = True
 
 # Create the bot and grab our private key/token from the Heroku environment variables.
-bot = commands.Bot(command_prefix = "$", intents = intents)
 bot = commands.Bot(command_prefix = "!", intents = intents)
 TOKEN = os.getenv("DISCORD_TOKEN")
 
@@ -72,6 +72,65 @@ async def on_member_update(before, after):
 async def ping(ctx):
 	await ctx.send("pong")
 	log(f"Sent a pong for a ping!")
+
+# Pull CSV of discord usernames from mysterium.net and try to apply a specific role to them. 
+@bot.command()
+async def bulkadd(ctx, url):
+	guild = ctx.guild
+	users = guild.members
+	attendeeRole = discord.utils.get(guild.roles, name="Mysterium Onsite")
+	statusChannel = discord.utils.get(guild.text_channels, name=_status_messages_channel)
+	usersNotFound = []
+	
+
+	# If user is Mysterium Staff then,
+	if discord.utils.get(guild.roles, name="Mysterium Staff") in ctx.author.roles:
+		# Announce it
+		message = f"User '{ctx.author}' just started the bulk add mysterium attendee role command."
+		log(message)
+		await statusChannel.send(message)
+
+		try:
+			# Pull in the CSV from the user-provided URL
+			with urlopen(url) as response:
+				# Unicode unfuckery
+				lines = (line.decode('utf-8') for line in response)
+				# Iterate over each line in the CSV and process
+				for row in csv.reader(lines):
+					# Split  Username#0000 into two in order to pass them to the Discord disambiguator
+					csvUser=row[0].split("#")
+					# Pass name and, if present, the discriminator (thanks for the "improvement", Discord...)
+					with discord.utils.get(users, name = csvUser[0], discriminator = (csvUser[1] if len(csvUser) == 2 else None)) as u:
+						if u is None:
+							# User as-presented isn't in Members list so 'get' call returned None. Add them to a list to report back later.
+							# TODO: Maybe if name & discriminator fail, try the call again with just name before failing?
+							usersNotFound.append(row[0])
+						else:
+							# User was found in member list so do the needful.
+							#TODO: I think this doesn't fail if the user already has the role? Discord and discord.py docs both don't address this.
+							await u.add_roles(attendeeRole)
+		except urllib.error.URLError as e:
+			# Oops, did you get the URL right??
+			message = f"Failed retrieving '{url}' with server response '{e.code}'."
+			log(message)
+			await statusChannel.send(message)
+
+		# Build a message to report back on failed users and send it:
+		if len(usersNotFound) > 0:
+			uNFMessage = "The following users were not found in the server:"
+			for userNotFound in usersNotFound:
+				uNFMessage = uNFMessage + "\n    " + userNotFound
+			log(uNFMessage)
+			await statusChannel.send(uNFMessage)
+
+		message = "The bulk add mysterium attendee role command has finished."
+		log(message)
+		await statusChannel.send(message)
+
+	else:
+		message = f"User '{ctx.author}' just tried to run the bulk add mysterium attendee role command, but doesn't have the right permissions."
+		log(message)
+		await statusChannel.send(message)
 
 log("Bot starting...")
 
